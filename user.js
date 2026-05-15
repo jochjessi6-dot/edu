dcaa38c334c152816da0124b10f68828a222d62b
jochjessi6-dot <jochjessi6@gmail.com>
2026-05-15 08:27:43 +0700

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-passport.use(new GoogleStrategy({
-  clientID: "YOUR_CLIENT_ID",
-  clientSecret: "YOUR_SECRET",
-  callbackURL: "/auth/google/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  return done(null, profile);
-}));
